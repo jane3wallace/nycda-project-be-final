dc3a0ca6e9899dca76db80d01349096b321843e8
jane3wallace <jane3wallace@gmail.com>
2015-09-10 21:59:38 -0400

    added name to devise user

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 gem 'devise'
 gem 'paperclip', '~> 4.2'
+# gem 'bootstrap-sass', '~> 3.3.5'
+# gem 'autoprefixer-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909210736) do
+ActiveRecord::Schema.define(version: 20150911005734) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20150909210736) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
