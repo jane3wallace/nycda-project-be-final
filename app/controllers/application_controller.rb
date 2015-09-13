e0ef257e5dbe6d336f32a22441ef963963361e8c
jane3wallace <jane3wallace@gmail.com>
2015-09-12 22:29:36 -0400

    updated all forms with bootstrap formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  # flash[:errors] = flash[:errors].to_a.concat resource.errors.full_messages
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :current_password, :avatar, :first_name, :last_name) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :current_password, :avatar, :first_name, :last_name) }
